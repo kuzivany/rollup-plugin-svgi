3aa765aaa6dc9381828e3f043c1c353e47951bbb
Kuzivakwashe <kuzisnare@gmail.com>
2019-01-07 19:28:00 +0200

    Change main entry points

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,12 @@
-import { join } from 'path';
-import { writeFileSync, existsSync, mkdirSync, copyFileSync } from 'fs';
-
-const pkg = require('./package');
-const outputDir = join('.', 'dist');
-
-// dist:
-const README = 'README.md';
-
-if ( !existsSync(outputDir) ) {
-	mkdirSync(outputDir);
-}
-
-delete pkg.scripts;
-delete pkg.devDependencies;
-writeFileSync(join(outputDir, 'package.json'), JSON.stringify(pkg, null, 2));
-copyFileSync(join('.', README), join(outputDir, README));
-// dist;
+import pkg from './package.json';
 
 export default {
-	input: 'index.js',
+	input: pkg.module,
 	output: [
 		{
-			file: join(outputDir, pkg.main),
+			file: pkg.main,
 			format: 'cjs'
-		},
-		{
-			file: join(outputDir, pkg.module),
-			format: 'esm'
 		}
-	]
+	],
+	external: Object.keys(pkg.dependencies),
 }
