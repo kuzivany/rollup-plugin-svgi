2f1f50f2a85b5030942193b1b02a82c466ae1c07
Kuzivakwashe <kuzisnare@gmail.com>
2019-01-07 18:50:20 +0200

    options.clean can now return Promises

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { createFilter } from "rollup-pluginutils";
+import { createFilter } from 'rollup-pluginutils';
 
 /**
  * Imports a SVG file and exports it as a module
  * with `default` pointing to a functional component.
  * @param {SVGiConfig} config
  */
-function svgi ({ options, exclude, include = '**/*.svg' }) {
+export default function svgi ({ options, exclude, include = '**/*.svg' }) {
 	if ( !(options && options.jsx) ) {
 		throw new Error("options.jsx is required");
 	}
@@ -22,8 +22,9 @@ function svgi ({ options, exclude, include = '**/*.svg' }) {
 			if ( !filter(id) ) return;
 
 			let library;
+			let cleanedSVG;
 			let factory = options.factory || null;
-			let $default = typeof options.default === "boolean"?
+			let isDefault = typeof options.default === "boolean"?
 				options.default:
 				true
 			;
@@ -32,19 +33,19 @@ function svgi ({ options, exclude, include = '**/*.svg' }) {
 					.replace(/\s*<\?xml[\s\S]+?\?>\s*/, "") // Remove XML declaration
 					.replace(/\s*<!DOCTYPE[\s\S]*?>\s*/i, "") // Remove DOCTYPE
 					.replace(/[a-z]+\:[a-z]+\s*=\s*"[\s\S]+?"/ig, "") // Remove namespaced attributes
-					.replace(/\s*<!\-\-[\s\S]*?\-\->\s*/i, "") // Remove comments
+					.replace(/\s*<!\-\-[\s\S]*?\-\->\s*/ig, "") // Remove comments
 				)
 			);
 
 			switch ( options.jsx ) {
 				case "preact":
 					factory = "h";
-					$default = false;
+					isDefault = false;
 				break;
 					
 				case "react":
-					factory = "React"
-					$default = true;
+					factory = "React";
+					isDefault = true;
 				break;
 			}
 
@@ -52,40 +53,56 @@ function svgi ({ options, exclude, include = '**/*.svg' }) {
 				throw new Error("options.factory couldn't be set from the provided options");
 			}
 
-			factory = $default? factory: `{ ${factory} }`;
-			library = `import ${factory} from '${options.jsx}';`
+			factory = isDefault? factory: `{ ${factory} }`;
+			library = `import ${factory} from '${options.jsx}';`;
+			cleanedSVG = clean(svg);
 
-			svg = clean(svg);
+			if ( typeof cleanedSVG !== "string" ) {
+				// Check whether clean returned a Promise
+				let isPromise = (
+					typeof cleanedSVG === "object" && cleanedSVG.then
+				);
 
-			// Add props:
-			svg = svg.replace(/<svg([\s\S]*?)>/i, "<svg$1 {...props}>");
+				if ( !isPromise ) {
+					throw new Error('options.clean did not return a string or Promise<string>');
+				}
+			} else {
+				cleanedSVG = Promise.resolve(cleanedSVG);
+			}
 
-			const output = {
-				code: (
-					`${library}\n` +
-					`export default ( props ) => (${svg});`
-				),
-				map: { mappings: '' }
-			};
-
-			return output;
+			return cleanedSVG.then(cleanedSVG => {
+				// Add props:
+				cleanedSVG = cleanedSVG.replace(/<svg([\s\S]*?)>/i, "<svg$1 {...props}>");
+	
+				return {
+					code: (
+						`${library}\n` +
+						`export default ( props ) => (${cleanedSVG});`
+					),
+					map: { mappings: '' }
+				};
+			})
 		}
 	};
 }
-
-export default svgi;
-
-/**
- * @typedef {Object} SVGiOptions
- * @property {"preact"|"react"|string} jsx `"preact"`, `"react"` or your chosen JSX library
- * @property {string} [factory] The variable/ function being imported
- * @property {boolean} [default] Whether the import is a default or not. Defaults to `true`
- * @property {(rawSVG:string)=>string} [clean] A function that prepares the SVG output for use in JSX
- */
 
 /**
  * @typedef {Object} SVGiConfig
  * @property {string|string[]} [exclude] Minimatch pattern(s) to exclude
  * @property {string|string[]} [include='**\/*.svg'] Minimatch pattern(s) to include
  * @property {SVGiOptions} options Map of options
+ */
+
+/**
+ * @typedef {Object} SVGiOptions
+ * @property {"preact"|"react"|string} jsx `"preact"`, `"react"` or your chosen JSX library
+ * @property {string} [factory] The variable/ function being imported
+ * @property {boolean} [default] Whether the import is a default or not. Defaults to `true`
+ * @property {SVGiCleanFunction} [clean] A function that prepares the SVG output for use in JSX
+ */
+
+/**
+ * @callback SVGiCleanFunction
+ * @param {string} rawSVG The raw SVG file contents as a string
+ * @returns {string|Promise<string>}
  */
