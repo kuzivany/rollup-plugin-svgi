ce0d76e739f4fb722d46df1cb99d79d951b1fd97
Kuzivakwashe <kuzisnare@gmail.com>
2019-02-09 19:13:01 +0200

    dangerously set SVG

--- src/rollup-plugin-svgi.mjs
+++ src/rollup-plugin-svgi.mjs
@@ -24,6 +24,7 @@ function svgi ({ options, exclude, include = '**/*.svg' }) {
 			let library;
 			let cleanedSVG;
 			let factory = options.factory || null;
+			let pragma = options.pragma || null;
 			let isDefault = typeof options.default === "boolean"?
 				options.default:
 				true
@@ -41,17 +42,23 @@ function svgi ({ options, exclude, include = '**/*.svg' }) {
 			switch ( options.jsx ) {
 				case "preact":
 					factory = "h";
+					pragma = factory;
 					isDefault = false;
 				break;
 					
 				case "react":
 					factory = "React";
+					pragma = factory + '.createElement'
 					isDefault = true;
 				break;
 			}
 
 			if ( !factory ) {
 				throw new Error("options.factory couldn't be set from the provided options");
+			}
+
+			if ( !pragma ) {
+				throw new Error("options.pragma couldn't be set from the provided options");
 			}
 
 			factory = isDefault? factory: `{ ${factory} }`;
@@ -73,12 +80,22 @@ function svgi ({ options, exclude, include = '**/*.svg' }) {
 
 			return cleanedSVG.then(cleanedSVG => {
 				// Add props:
-				cleanedSVG = cleanedSVG.replace(/<svg([\s\S]*?)>/i, "<svg$1 {...props}>");
+				let props = cleanedSVG.match(/\<svg([\s\S]*?)\>/i, "$1")[1] || '';
+				props = `{ ${props.replace(/\s*([^=]+)\s*=\s*("[^"]+")/g, "'$1': $2,")} }`
+				cleanedSVG = cleanedSVG.replace(/^\<svg([\s\S]*?)\>|\s*\<\/svg\>\s*$/ig, '');
+
+				console.log('PROPS>', props)
 	
 				return {
 					code: (
 						`${library}\n` +
-						`export default ( props ) => (${cleanedSVG});`
+						`export default function ( props ) {` + // 
+							`props.dangerouslySetInnerHTML = { __html: ${JSON.stringify(cleanedSVG)} };` +
+							`return ${pragma}(` +
+								`'svg',` +
+								`Object.assign(${props}, props)` +
+							`);` +
+						`}`
 					),
 					map: { mappings: '' }
 				};
@@ -99,8 +116,9 @@ export default svgi;
 /**
  * @typedef {Object} SVGiOptions
  * @property {"preact"|"react"|string} jsx `"preact"`, `"react"` or your chosen JSX library
- * @property {string} [factory] The variable/ function being imported
- * @property {boolean} [default] Whether the import is a default or not. Defaults to `true`
+ * @property {string} [factory] The default or named exports of the chosen library
+ * @property {string} [pragma] The JSX pragma
+ * @property {boolean} [default] Whether the `export` is a `default` or not. Defaults to `true`
  * @property {SVGiCleanFunction} [clean] A function that prepares the SVG output for use in JSX
  */
 
